f2974989bdeaccfe4a6c1408adace0ee96604fc2
mairako <mkotsovoulou@acg.edu>
2020-11-03 15:42:58 +0000

    PHP database Query

--- phpdb/db.php
+++ phpdb/db.php
@@ -7,7 +7,7 @@ try{
         ";port=" . DB_PORT,DB_USER,DB_PASS);
 
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "connected" . '<br/>';
+    //echo "connected" . '<br/>';
 }
 catch (Exception $e){
     echo "Database Connection Error.";
--- phpdb/displaystudents.php
+++ phpdb/displaystudents.php
@@ -4,14 +4,20 @@
   </head>
   <body>
     <h1>
-       list of students
+       List of students
     </h1>
     
     <?php 
        include "db.php";
        $query = $db->query("select * from student");
        $results = $query->fetchALL(PDO::FETCH_ASSOC);
-       var_dump($results);
+      // var_dump($results);
+      echo '<ol>';
+    foreach ($results as $row)
+    {
+      echo '<li>'. $row['first_name'] . " " . $row['last_name']. '</li>';
+    }
+     echo '</ol>';
     ?>
     
   </body>
