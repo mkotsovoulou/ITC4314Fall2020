77d24053a27dfda4b7f6e0a88e57b2b255b26d55
mairako <mkotsovoulou@acg.edu>
2020-11-05 15:59:41 +0000

    Now

--- phpdb/displaybooks.php
+++ phpdb/displaybooks.php
@@ -4,8 +4,8 @@
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   </head>
   <body>
-    <h class="display-1">
-       List of Books
+    <h1 class="display-1">
+       List of Books Now
     </h1>
     
    <?php
@@ -20,11 +20,11 @@
        <tbody>
     
     <?php    
-        foreach($results as $row)
-        {
-               echo "<tr><td>".$row['id']."</td><td>".$row['title']."</td><td>".$row['quantity']." items</td><td>".$row['price']." $</td><td>".$row['description']."</td><td>".$row['date_added']."</td><td><img src='images/".$row['image_filename']."' width='50px'></td></tr>";
+foreach($results as $row)
+ {
+     echo "<tr><td>".$row['id']."</td><td>".$row['title']."</td><td>".$row['quantity']." items</td><td>".$row['price']." $</td><td>".$row['description']."</td><td>".$row['date_added']."</td><td><img src='images/".$row['image_filename']."' width='50px'></td></tr>";
         } ?>
-       </tbody>
+ </tbody>
  </table>  
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
