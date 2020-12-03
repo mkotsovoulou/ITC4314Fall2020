e969b1bbfbcf12842a8a1a21b2f4d147d89ff6cd
mairako <mkotsovoulou@acg.edu>
2020-12-03 15:40:32 +0000

    usercount

--- PHPdb/usercount.php
+++ PHPdb/usercount.php
@@ -4,9 +4,18 @@ header("Content-Type: application/json");
 $username = $_POST['p_username'];
 $message = Array();
 
+try {
+  $results = $db->prepare("insert into usercount (username, countt) values (?,?)");
+  $results->bindValue(1,$username);
+  $results->bindValue(2,1);
+  $results->execute();
+  $message["text"]=$username. " Inserted and Counted!!!";
+} catch (PDOException $e) {
+   $results = $db->prepare("update usercount set countt = countt+1 where username = ?");
+   $results->bindValue(1,$username);
+   $results->execute();
+   $message["text"]=$username. " Counted!!!";
+}
 
-
-
-$message["text"]=$username. " Counted!!!";
 echo json_encode($message);
 ?>
